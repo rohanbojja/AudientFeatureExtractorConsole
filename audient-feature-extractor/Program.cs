dfc13254f7947aaece23853d99b702590e381c48
Rohan Bojja <rohanbojja@outlook.com>
2020-04-07 23:00:31 +0530

    Extracting MFCC, time-domain and spectral features and write them to a  CSV file.

--- audient-feature-extractor/Program.cs
+++ audient-feature-extractor/Program.cs
@@ -3,11 +3,14 @@ using NWaves.FeatureExtractors;
 using NWaves.FeatureExtractors.Multi;
 using NWaves.FeatureExtractors.Options;
 using NWaves.FeatureExtractors.Serializers;
+using NWaves.Features;
 using NWaves.Signals;
+using NWaves.Transforms;
 using NWaves.Windows;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace audient_feature_extractor
 {
@@ -16,11 +19,12 @@ namespace audient_feature_extractor
         private static IList<float[]> mfccVectors;
         private static IList<float[]> tdVectors;
 
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static void Main(string[] args)
         {
             DiscreteSignal signal;
 
             // load
+            var samplingRate = 16000;
             var mfccOptions = new MfccOptions
             {
                 SamplingRate = 16000,
@@ -37,34 +41,41 @@ namespace audient_feature_extractor
                 FrameDuration = 0.025,
                 HopDuration = 0.010
             };
+
+            
+
             var tdExtractor = new TimeDomainFeaturesExtractor(opts);
 
             var mfccExtractor = new MfccExtractor(mfccOptions);
 
-            var folders = Directory.GetFiles(Path.Combine(Environment.CurrentDirectory, "Dataset"));
+            var folders = Directory.GetDirectories(Path.Combine(Environment.CurrentDirectory, "Dataset"));
+            Console.WriteLine($"Started!");
             using (var writer = File.CreateText(Path.Combine(Environment.CurrentDirectory, "Data.csv")))
             {
                 //Write header
                 var main_header = "genre,";
                 main_header += String.Join(",", mfccExtractor.FeatureDescriptions);
-                
+                main_header += ",";
+                main_header += String.Join(",", tdExtractor.FeatureDescriptions);
+                main_header += ",centroid,spread,flatness,noiseness,roloff,crest,decrease,entropy";
+                writer.WriteLine(main_header);
                 string feature_string = String.Empty;
                 foreach (var folder in folders)
                 {
-                    feature_string = $"{folder},";
+                    var f_name = new DirectoryInfo(folder).Name;
                     var files = Directory.GetFiles(Path.Combine(Environment.CurrentDirectory, "Dataset", folder));
                     //Write the genre label here
-
-                    writer.WriteLine(main_header);
+                    Console.WriteLine($"{f_name}");
                     foreach (var filename in files)
                     {
+                        feature_string = String.Empty;
+                        feature_string = $"{f_name},";
                         //MFCC
                         var avg_vec_mfcc = new List<float>(14);
                         //TD Features
                         var avg_vec_td = new List<float>(4);
                         //Spectral features
-
-
+                        var avg_vec_spect = new List<float>(10);
 
                         for (var i = 0; i < 13; i++)
                         {
@@ -75,6 +86,12 @@ namespace audient_feature_extractor
                             avg_vec_td.Add(0f);
                         }
 
+                        for (var i = 0; i < 10; i++)
+                        {
+                            avg_vec_spect.Add(0f);
+                        }
+
+                        string specFeatures = String.Empty;
                         using (var stream = new FileStream(Path.Combine(Environment.CurrentDirectory, "Dataset", filename), FileMode.Open))
                         {
                             var waveFile = new WaveFile(stream);
@@ -82,10 +99,26 @@ namespace audient_feature_extractor
                             //Compute MFCC
                             tdVectors = tdExtractor.ComputeFrom(signal);
                             mfccVectors = mfccExtractor.ComputeFrom(signal);
+                            var fft = new Fft(1024);
+                            var fftSize = 1024;
+                            var resolution = (float)samplingRate / fftSize;
 
+                            var frequencies = Enumerable.Range(0, fftSize / 2 + 1)
+                                                        .Select(f => f * resolution)
+                                                        .ToArray();
+
+                            var spectrum = new Fft(fftSize).MagnitudeSpectrum(signal).Samples;
+
+                            var centroid = Spectral.Centroid(spectrum, frequencies);
+                            var spread = Spectral.Spread(spectrum, frequencies);
+                            var flatness = Spectral.Flatness(spectrum, 0);
+                            var noiseness = Spectral.Noiseness(spectrum, frequencies, 3000);
+                            var rolloff = Spectral.Rolloff(spectrum, frequencies, 0.85f);
+                            var crest = Spectral.Crest(spectrum);
+                            var decrease = Spectral.Decrease(spectrum);
+                            var entropy = Spectral.Entropy(spectrum);
+                            specFeatures = $"{centroid},{spread},{flatness},{noiseness},{rolloff},{crest},{decrease},{entropy}";
                         }
-
-                        Console.WriteLine(tdVectors.Count);
 
                         //Write label here TODO
 
@@ -104,25 +137,28 @@ namespace audient_feature_extractor
                                 avg_vec_td[i] += inst[i];
                             }
                         }
-
-                        // Write MFCCs
+                        
                         for (var i = 0; i < 13; i++)
                         {
                             avg_vec_mfcc[i] /= mfccVectors.Count;
                         }
 
-                        for (var i = 0; i < 13; i++)
+                        for (var i = 0; i < 4; i++)
                         {
                             avg_vec_td[i] /= tdVectors.Count;
                         }
 
-                        feature_string = String.Join(",", avg_vec_mfcc);
+
+                        // Write MFCCs
+                        feature_string += String.Join(",", avg_vec_mfcc);
+                        feature_string += ",";
                         feature_string += String.Join(",", avg_vec_td);
                         //Write Spectral features as well
-
-
-                        writer.WriteLine(feature_string);
-                        Console.WriteLine($"{filename}");
+                        feature_string += ",";
+                        feature_string += specFeatures;
+                        writer.WriteLine(feature_string); 
+                        var file_name = new DirectoryInfo(filename).Name;
+                        Console.WriteLine($"{file_name}");
                     }
                 }
             }
